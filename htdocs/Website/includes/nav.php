bcfc648c2a933e12af3edf1df20dda8336cff2d7
Kunal Singh <ks820177@gmail.com>
2024-04-15 08:16:50 +0530

    delete

--- htdocs/Website/includes/nav.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <?php
-        foreach ($navItems as $item) {
-            // Use braces around array keys to properly reference them
-            echo "<li><a href=\"{$item['slug']}\">{$item['title']}</a></li>";
-        }
-    ?>
-</ul>
--- htdocs/assets/includes/copyright.php
+++
@@ -1,2 +0,0 @@
-<h4>Code Dynamic Websites with PHP</h4>
-<h5>Copyright &copy;<?php echo date('Y');?> <a href="#" >Kunal Singh</a>
